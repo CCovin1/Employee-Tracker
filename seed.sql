d68bed1e21065c314ba130a377129d85dc24aad1
CCovin1 <caleb.covin@gmail.com>
2023-08-07 11:06:53 -0500

    added names to the seeds

--- seed.sql
+++ seed.sql
@@ -16,3 +16,23 @@ VALUES
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
+('John', 'Doe', 1, NULL),
+('Jane', 'Smith', 2, 1),
+('Oliver', 'Johnson', 3, NULL),
+('Emma', 'Williams', 4, 3),
+('Noah', 'Jones', 5, NULL),
+('Ava', 'Brown', 6, 5),
+('Liam', 'Davis', 7, NULL),
+('Sophia', 'Miller', 8, 7),
+('Ethan', 'Wilson', 2, 1),
+('Isabella', 'Moore', 4, 3),
+('Mason', 'Taylor', 6, 5),
+('Mia', 'Anderson', 8, 7),
+('Lucas', 'Thomas', 1, 1),
+('Charlotte', 'Jackson', 3, 3),
+('Aiden', 'White', 5, 5),
+('Harper', 'Harris', 7, 7),
+('Elijah', 'Martin', 2, 1),
+('Amelia', 'Thompson', 4, 3),
+('James', 'Garcia', 6, 5),
+('Benjamin', 'Martinez', 8, 7);
