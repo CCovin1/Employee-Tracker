5ea4a72c51d19d610271e949bbdd268e2cb541f5
CCovin1 <caleb.covin@gmail.com>
2023-08-07 11:00:29 -0500

    added more to the js

--- index.js
+++ index.js
@@ -38,3 +38,53 @@ const addDepartment = async () => {
     type: 'input',
     message: 'What is the name of the department?'
   });
+
+  await db.query('INSERT INTO department SET ?', { name: answer.name });
+  console.log('Added new department');
+  await start();
+};
+
+const start = async () => {
+  const { action } = await inquirer.prompt({
+    name: 'action',
+    type: 'list',
+    message: 'What would you like to do?',
+    choices: [
+      'View all departments',
+      'View all roles',
+      'View all employees',
+      'Add a department',
+      'Add a role',
+      'Add an employee',
+      'Update an employee role',
+      'Exit'
+    ],
+  });
+
+  switch (action) {
+    case 'View all departments':
+      await viewDepartments();
+      break;
+    case 'View all roles':
+      await viewRoles();
+      break;
+    case 'View all employees':
+      await viewEmployees();
+      break;
+    case 'Add a department':
+      await addDepartment();
+      break;
+    case 'Add a role':
+      await addRole();
+      break;
+    case 'Add an employee':
+      await addEmployee();
+      break;
+    case 'Update an employee role':
+      await updateEmployeeRole();
+      break;
+    default:
+      db.end();
+  }
+};
+
